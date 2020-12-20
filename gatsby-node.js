d9f55e304d49e3dba34948cca5d8ff40c1962803
sherwin <sherwin.web.ph@tripleonetech.net>
2020-12-21 02:24:05 +0800

    first commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,43 @@
  */
 
 // You can delete this file if you're not using it
+
+// create page dynamically
+// exports.createPages = async ({ graphql,actions }) => {
+//   const {createPage} = actions
+//   const result = await graphql(`
+//     query GetLocations {
+//       locations:allContentfulLocations{
+//         nodes{
+//           slug
+//         }
+//       }
+//     }
+//   `)  
+// }
+
+
+const path = require('path')
+// Create blog pages dynamically
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const result = await graphql(`
+  query GetLocations {
+      locations:allContentfulLocations{
+        nodes{
+          slug
+        }
+      }
+    }
+  `)
+
+  result.data.locations.nodes.forEach(element => {
+    createPage({
+      path: `/explore/${element.slug}`,
+      component: path.resolve(`src/templates/location-template.js`),
+      context:{
+        title: element.slug
+      }
+    })
+  });
+}
